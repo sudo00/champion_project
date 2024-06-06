ecd2741090e7dccea7a0862e7b171c0f457346f3
Danil Bolotov <DaVBolotov@prm.beeline.ru>
2024-06-06 22:29:35 +0500

    [Backend] Auth

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,76 @@
-from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS  
 from os import environ
+from flask_cors import CORS  
+from flask import Flask, request, jsonify, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from jwt import encode, decode  # Импорт функций из модулей 
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = 'your_secret_key'  # Замените на свой секретный ключ
+app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(minutes=30)  # Время жизни токена
 db = SQLAlchemy(app)
 
-#create a test route
-@app.route('/test', methods=['GET'])
-def test():
-  return make_response(jsonify({'message': 'test route'}), 200)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
+
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = None
+        try:
+            if 'Authorization' in request.headers:
+                token = request.headers['Authorization'].split(" ")[1]
+            if not token:
+                return jsonify({'message': 'Token is missing!'}), 401
+            data = decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            current_user = User.query.filter_by(id=data['id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return func(current_user, *args, **kwargs)
+    return decorated
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    if not data or not 'username' in data or not 'password' in data:
+        return jsonify({'message': 'Missing username or password'}), 400
+    username = data['username']
+    password = data['password']
+    hashed_password = generate_password_hash(password)
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    token = encode({'id': new_user.id, 'exp': datetime.datetime.utcnow() + app.config['JWT_EXPIRATION_DELTA']}, app.config['SECRET_KEY'], algorithm='HS256')
+    return jsonify({'token': token}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data or not 'username' in data or not 'password' in data:
+        return jsonify({'message': 'Missing username or password'}), 400
+    username = data['username']
+    password = data['password']
+    user = User.query.filter_by(username=username).first()
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({'message': 'Invalid credentials'}), 401
+    token = encode({'id': user.id, 'exp': datetime.datetime.utcnow() + app.config['JWT_EXPIRATION_DELTA']}, app.config['SECRET_KEY'], algorithm='HS256')
+    return jsonify({'token': token}), 200
+
+@app.route('/protected')
+@token_required
+def protected(current_user):
+    return jsonify({'message': 'You are authorized!', 'user_id': current_user.id})
+
+if __name__ == '__main__':
+    app.run(debug=True)
