8f4c89b4ba0913deb4993eb557d9d630c1e60d25
Denis <dshebut@rambler.ru>
2024-06-12 15:02:34 +0300

    Создание маски

--- backend/banner_plus_inpaint/generate_mask.py
+++ backend/banner_plus_inpaint/generate_mask.py
@@ -1,4 +1,5 @@
-from PIL import Image
+from PIL import Image, ImageDraw
+
 
 def generate_mask(
         top_left: tuple[int, int],
@@ -7,4 +8,14 @@ def generate_mask(
         output_path: str,
 ):
     original_image = Image.open(input_path)
-    mask = Image.new(mode="RGB",size=(original_image.width, original_image.height),color=(0,0,0))
+    mask = Image.new(mode="RGB", size=(original_image.width, original_image.height), color=(0, 0, 0))
+    draw = ImageDraw.Draw(mask)
+    draw.rectangle((top_left, bottom_right), fill="white")
+    mask.save(output_path)
+
+generate_mask(
+    top_left=(50, 50),
+    bottom_right=(150, 150),
+    input_path="./photo_2024-06-11_12-17-48.jpg",
+    output_path="./mask.png"
+)
